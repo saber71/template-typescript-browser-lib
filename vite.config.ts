6569c3cfb2e18889296dba771a3ef46603abe3c1
saber71 <1533357473@qq.com>
2024-11-13 20:13:48 +0800

    refactor(remote-addon): 重构远程插件架构并添加 Vue 支持
    - 移除了 remote-addon-dom 相关代码，为 Vue 插件让路
    - 新增了 remote-addon-vue 插件，提供对 Vue 的支持
    - 更新了项目结构和配置，以适应新的插件架构

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
       entry: resolve(__dirname, "src/index.ts"),
       fileName: "index",
       formats: ["es"]
-    }
+    },
+    outDir: resolve(__dirname, "dist")
   }
 })
